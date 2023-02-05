42088504ed31a71ccb48042203147207be92f24d
xoz5 <113217028+xoz5@users.noreply.github.com>
2023-02-04 19:50:26 -0800

    Make button switch grid size

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let gridItemCount = 16;
 const gridContainer = document.querySelector('.grid-container');
-
+const btn = document.querySelector('button');
+btn.addEventListener('click', changeGridSize);
 addDivsToGridContainer(gridItemCount);
 
 function addDivsToGridContainer(gridItemCount) {
@@ -22,4 +23,16 @@ function addDivsToGridContainer(gridItemCount) {
 function changeGridColorByHover(divHovered) {
     let randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
     divHovered.target.style.backgroundColor = randomColor;
+}
+
+function changeGridSize() {
+    let gridSizePreference = 0;
+
+    do {
+        gridSizePreference = prompt('Input a grid size of less than or equal to 100');
+    } while(gridSizePreference > 100);
+
+    gridContainer.setAttribute('style', `grid-template-rows: repeat(${gridSizePreference}, auto); grid-template-columns: repeat(${gridSizePreference},auto);`);
+
+    addDivsToGridContainer(gridSizePreference);
 }
